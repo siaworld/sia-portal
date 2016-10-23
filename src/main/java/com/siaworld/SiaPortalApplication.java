312abb4fee3126da73466d6bfcdb674e85e3aaed
siaworld <hjbaek0314@gmail.com>
2016-10-23 23:03:48 +0900

    Initial setup
    Initial setup

---
+++ src/main/java/com/siaworld/SiaPortalApplication.java
@@ -0,0 +1,12 @@
+package com.siaworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiaPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiaPortalApplication.class, args);
+	}
+}
